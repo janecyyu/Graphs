200f7167e225f67df04749643983ede21c654504
janecyyu <janecyyu@gmail.com>
2020-07-14 15:36:27 -0700

    except 7 8 9

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -5,13 +5,23 @@ def earliest_ancestor(ancestors, starting_node):
     q = Queue()
     q.enqueue([starting_node])
     visited = set()
+    v = starting_node
     while q.size() > 0:
         path = q.dequeue()
         v = path[-1]
         if v not in visited:
             visited.add(v)
             print("visited:", v)
-            # for next_node in get_neighbors(ancestors, v):
+            for next_node in get_neighbors(ancestors, v):
+                new_arr = path.copy()
+                new_arr.append(next_node)
+                q.enqueue(new_arr)
+
+    if v == starting_node:
+        print(-1)
+        return -1
+    print(v)
+    return v
 
 
 def get_neighbors(ancestors, node):
@@ -25,13 +35,19 @@ def get_neighbors(ancestors, node):
         if y == node:
             # add to neighbors
             neighbors.append(x)
-        else:
-            return None
-    print("my neighbors", neighbors)
+
+    # print("my neighbors", neighbors)
     return neighbors
 
 
-test_ancestors = [(1, 3), (2, 3), (10, 1)]
-earliest_ancestor(test_ancestors, 3)
+# test_ancestors = [(1, 3), (2, 3), (10, 1)]
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+                  (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
-# get_neighbors(test_ancestors, 3)
+
+earliest_ancestor(test_ancestors, 11)
+earliest_ancestor(test_ancestors, 7)
+earliest_ancestor(test_ancestors, 8)
+earliest_ancestor(test_ancestors, 9)
+
+# print(get_neighbors(test_ancestors, 3))

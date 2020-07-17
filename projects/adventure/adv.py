9f4311ddec7391cf5262195a13850820c6680489
janecyyu <janecyyu@gmail.com>
2020-07-16 17:09:40 -0700

    break at the end

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -29,26 +29,50 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n', "s", "s", "s", "s", "s"]
 traversal_path = []
-# q = Queue()
-# visited = set()
-# q.enqueue([0])
-# while q.size() > 0:
-#     path = q.dequeue()
-#     u = path[-1]
-#     if u not in visited:
-#         visited.add(u)
-#         for neighbor in u.get_exits():
-#             print("u", u)
-#             path_copy = list(u)
-#             room_name = u.get_room_in_direction(neighbor)
-#             path_copy.append(room_name)
-#             q.enqueue(path_copy)
-#             print(q.queue)
+NoneType = type(None)
+q = Queue()
+visited = set()
+# print(type(player.current_room))
+q.enqueue(player.current_room)
+while q.size() > 0:
+    path = q.dequeue()
+    print("type path", type(path))
+    # str = type(path)
+    print(type(path) is NoneType)
+    if(type(path) is NoneType):
+        print("break??")
+        break
+    q.enqueue(path.get_room_in_direction("n"))
+    # u = path[-1]
 
+    # if path not in visited:
+    #     visited.add(path)
+    #     # get neighbors
+    #     directions = path.get_exits_string()
+    #     # grab n,e,s,w only
+    #     directions = directions[8:-1]
+    #     directions = directions.replace(',', '').replace(' ', '')
+    #     for d in directions:
+    #         # print(d)
+    #         path_copy = path
+    #         new_room = path.get_room_in_direction(d)
+    #         path_copy.append(new_room)
+    #         q.enqueue(path_copy)
+# print(q.queue)
+print("break")
+
+# if u not in visited:
+#     visited.add(u)
+#     for neighbor in u.get_exits():
+#         print("u", u)
+#         path_copy = list(u)
+#         room_name = u.get_room_in_direction(neighbor)
+#         path_copy.append(room_name)
+#         q.enqueue(path_copy)
+#         print(q.queue)
 
 # def get_neighbors(room):
 #     return room.get_exits()
-
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -71,6 +95,8 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
+t = player.current_room  # Exits: [n, s, w, e]
+print("t", type(t))
 # t = player.current_room.get_exits_string() # Exits: [n, s, w, e]
 # t = player.current_room.name  # Room 0
 # t = player.current_room.id  # 0
